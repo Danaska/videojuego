bd3d6ce0657e7bf244cfaefe81d9c3081489aefe
danaska <danaska.alonso@gmail.com>
2020-03-30 17:33:15 +0200

    Realizada la refactorización extrayendo cadenas de texto y métodos.

--- videoJuego/src/net/instituto/desa/Game.java
+++ videoJuego/src/net/instituto/desa/Game.java
@@ -1,20 +1,40 @@
 package net.instituto.desa;
 
 public class Game {
+	private static final String ABAJO = "Abajo";
+	private static final String ARRIBA = "Arriba";
+	private static final String IZQUIERDA = "Izquierda";
+	private static final String DERECHA = "Derecha";
 	Jugador j;
 
 	public void movimiento(String m) {
-		if (m.equalsIgnoreCase("Derecha")) {
-			j.setX(j.getX() + 1);
+		if (m.equalsIgnoreCase(DERECHA)) {
+			derecha();
 		}
-		if (m.equalsIgnoreCase("Izquierda")) {
-			j.setX(j.getX() - 1);
+		if (m.equalsIgnoreCase(IZQUIERDA)) {
+			izquierda();
 		}
-		if (m.equalsIgnoreCase("Arriba")) {
-			j.setY(j.getY() - 1);
+		if (m.equalsIgnoreCase(ARRIBA)) {
+			arriba();
 		}
-		if (m.equalsIgnoreCase("Abajo")) {
-			j.setY(j.getY() + 1);
+		if (m.equalsIgnoreCase(ABAJO)) {
+			abajo();
 		}
+	}
+
+	public void abajo() {
+		j.setY(j.getY() + 1);
+	}
+
+	public void arriba() {
+		j.setY(j.getY() - 1);
+	}
+
+	public void izquierda() {
+		j.setX(j.getX() - 1);
+	}
+
+	public void derecha() {
+		j.setX(j.getX() + 1);
 	}
 }
